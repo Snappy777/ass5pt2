3648197f90daf7c438400cef994da40b74557974
Snappy7 <97709582+Snappy777@users.noreply.github.com>
2023-10-22 20:36:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+  $(document).ready(function(){
+	$("#flip").click(function(){
+	  $("#panel").slideToggle("slow");
+	});
+  });
+ 
+ 
+
+  $(".disappear").click(function(){
+	$(this).hide("slow");
+  });
